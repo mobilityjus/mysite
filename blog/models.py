1e7f5847735cf76455756c2231f8c68cf6bc55fb
Justin Masuka <mobilityjus@gmail.com>
2020-08-31 15:15:40 +0300

    Add feature to approve comments on blog posts

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+    
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
 
     # create a string representation for the method
 
@@ -25,6 +28,11 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default = timezone.now)
+    approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved = True
+        self.save()
 
     def __str__(self):
         return self.text
